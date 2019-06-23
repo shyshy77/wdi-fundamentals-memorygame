171f87dc1803e0ad934754a47f250d51c3737763
Sharona Obillos <sharonaobillos@Sharonas-Air.fios-router.home>
2019-06-22 23:08:47 -0400

    created game logic functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,30 @@
 console.log("Up and running!");
-/*
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-*/
-//console.log("User flipped " + cardOne);
-//console.log("User flipped " + cardThree);
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-//console.log("User flipped " + cardTwo);
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
 
 
+function flipCard (cardId) {
+//var cardId = this.setAttribute('data-id');
 if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
          alert("You found a match!");
     } else {
         alert("Sorry, try again.");
     }
+
+console.log("User flipped" + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+checkForMatch(0);
 }
+}
+
