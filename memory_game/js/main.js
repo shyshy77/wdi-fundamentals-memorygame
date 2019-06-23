beda1aeaf381fbf96b12bae77a01ac2d63d00f3b
Sharona Obillos <sharonaobillos@Sharonas-Air.fios-router.home>
2019-06-23 12:16:16 -0400

    created game logic objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,30 +1,51 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
-}
+  		alert("You found a match!");
+	} else {
+  		alert("Sorry, try again.");
+	}
+};
 
 
-function flipCard (cardId) {
-//var cardId = this.setAttribute('data-id');
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-         alert("You found a match!");
-    } else {
-        alert("Sorry, try again.");
-    }
+function flipCard() {
+	var cardId = this.setAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+console.log("User flipped" + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 
-console.log("User flipped" + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
 
-checkForMatch(0);
-}
 }
 
